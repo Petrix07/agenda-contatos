55a7f10fdf292131078b1bb82a5c0435880310d3
Petrixx <75099491+Petrix07@users.noreply.github.com>
2023-05-02 23:44:41 -0300

    Implementado o processamento de inclusão de pessoas

--- src/Controller/PessoaController.php
+++ src/Controller/PessoaController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
-use App\Form\PessoaType,
+use App\Entity\Pessoa,
+    App\Form\PessoaType,
     Symfony\Bundle\FrameworkBundle\Controller\AbstractController,
     Symfony\Component\HttpFoundation\Response,
     Doctrine\ORM\EntityManagerInterface,
-    Symfony\Component\Routing\Annotation\Route;
-
+    Symfony\Component\Form\FormInterface,
+    Symfony\Component\HttpFoundation\Request;
+use App\Repository\PessoaRepository;
 
 /**
  * Controller da entidade "Pessoa"
@@ -25,17 +27,56 @@ class PessoaController extends AbstractController
     }
 
     /**
-     * @Route("/pessoa/cadastrar", name="categoria_cadastrar")
+     * @param Request $oRequisicao
+     * @param EntityManagerInterface $oEm
+     * @return Response
      */
-    public function cadastrar(): Response
+    public function cadastrar(Request $oRequisicao, EntityManagerInterface $oEm): Response
     {
-        return $this->renderForm(self::pathFormCadastroPessoa, $this->getDadosFormularioCadastroPessoa());
+        $oPessoa = new Pessoa();
+        $oFormulario = $this->createForm(PessoaType::class, $oPessoa);
+        $oFormulario->handleRequest($oRequisicao);
+
+        if ($this->permiteExecucaoBanco($oFormulario)) {
+            if ($this->validaInformacoesRegistro($oPessoa, $oEm)) {
+                $oEm->persist($oPessoa);
+                $oEm->flush();
+                $this->addFlash('info', 'Registro incluído com sucesso!');
+            } else {
+                $this->addFlash('aviso', "Já existe um registro cadastrado com o CPF: {$oPessoa->getCpf()}. Para continuar a inclusão, altere o CPF informado.");
+            }
+        }
+
+        return $this->renderForm(self::pathFormCadastroPessoa, ['titulo' => 'Adicionar nova Pessoa', 'formulario' => $oFormulario]);
     }
 
-    private function getDadosFormularioCadastroPessoa(): array {
-        return [
-            'titulo'     => 'Adicionar nova Pessoa',
-            'formulario' => $this->createForm(PessoaType::class)
-        ];
+    /**
+     * Verifica se a condição do formulário permite a execução no banco de dados
+     * @return bool
+     */
+    private function permiteExecucaoBanco(FormInterface $oFormulario): bool
+    {
+        return $oFormulario->isSubmitted() && $oFormulario->isValid();
+    }
+
+    /**
+     * Aplica as validações da regra de negócio sob o registro 
+     * @return bool
+     */
+    private function validaInformacoesRegistro(Pessoa $oPessoa, EntityManagerInterface $oEm)
+    {
+        return $this->isNovoRegistroCpf($oPessoa, $oEm);
+    }
+
+    /**
+     * Verifica se o CPF informado não está associado a outro registro
+     * @return bool
+     */
+    private function isNovoRegistroCpf(Pessoa $oPessoa, EntityManagerInterface $oEm)
+    {
+        $xRetorno = $oEm->getRepository(Pessoa::class)->findOneBy([
+            'cpf' => $oPessoa->getCpf(),
+        ]);
+        return $xRetorno == null;
     }
 }
