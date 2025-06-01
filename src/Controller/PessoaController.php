4e72a846ad9807d5791b23d312bfb586791c12eb
ianpatricck <ianpatrick499@gmail.com>
2025-05-31 21:48:35 -0300

    Regra de negócio refatorada e incluido PessoaService

--- src/Controller/PessoaController.php
+++ src/Controller/PessoaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\PessoaService;
 use App\Entity\Pessoa,
     App\Form\PessoaType,
     Symfony\Bundle\FrameworkBundle\Controller\AbstractController,
@@ -13,161 +14,158 @@ use App\Entity\Pessoa,
 
 /**
  * Controller da entidade "Pessoa"
- * @author - Luiz Fernando Petris 
+ * @author - Luiz Fernando Petris
  * @since - 02/05/2023
  */
 class PessoaController extends AbstractController
 {
-    const pathConsultaPessoa     = 'pessoa/index.html.twig',
-          pathFormCadastroPessoa = 'pessoa/form.html.twig';
-
     /**
      * Renderiza a tela contendo a consulta de pessoas
-     * @param PessoaRepository $oPessoaRepo
+     * @param PessoaRepository $pessoaRepository
      */
-    public function index(PessoaRepository $oPessoaRepo): Response
+    public function index(PessoaRepository $pessoaRepository): Response
     {
-        $aDados = [
-            'titulo'  => 'Consulta de Pessoas',
-            'pessoas' => $oPessoaRepo->findAll()
+        $dados = [
+            "titulo" => "Consulta de Pessoas",
+            "pessoas" => $pessoaRepository->findAll(),
         ];
-        return $this->render(self::pathConsultaPessoa, $aDados);
+
+        return $this->render("pessoa/index.html.twig", $dados);
     }
 
     /**
      * Método que realiza o cadastro de uma nova pessoa
-     * @param Request $oRequisicao
-     * @param EntityManagerInterface $oEm
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
      * @return Response
      */
-    public function cadastrar(Request $oRequisicao, EntityManagerInterface $oEm): Response
-    {
-        $oPessoa = new Pessoa();
-        $oFormulario = $this->getFormularioFromPessoa($oRequisicao, $oPessoa);
+    public function cadastrar(
+        Request $request,
+        PessoaService $pessoaService,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $pessoa = new Pessoa();
+        $formulario = $this->getFormularioFromPessoa($request, $pessoa);
 
-        if ($this->permiteExecucaoBanco($oFormulario)) {
-            if ($this->validaInformacoesRegistroInclusao($oPessoa, $oEm)) {
-                $oEm->persist($oPessoa);
-                $oEm->flush();
-                $this->addFlash('info', 'Registro incluído com sucesso!');
+        if ($formulario->isSubmitted() && $formulario->isValid()) {
+            if (!$pessoaService->validaFormatoCpf($pessoa->getCpf())) {
+                $this->addFlash(
+                    "aviso",
+                    "O formato do CPF inserido não é válido. Para continuar a alteração, informe outro CPF."
+                );
+            } elseif (!$this->verificaCpfExiste($pessoa, $entityManager)) {
+                $this->addFlash(
+                    "aviso",
+                    "Já existe um registro cadastrado com o CPF: {$pessoa->getCpf()}. Para continuar a inclusão, altere o CPF informado."
+                );
             } else {
-                $this->addFlash('aviso', "Já existe um registro cadastrado com o CPF: {$oPessoa->getCpf()}. Para continuar a inclusão, altere o CPF informado.");
+                $entityManager->persist($pessoa);
+                $entityManager->flush();
+                $this->addFlash("info", "Registro incluído com sucesso!");
             }
         }
 
-        return $this->renderForm(self::pathFormCadastroPessoa, ['titulo' => 'Adicionar nova Pessoa', 'formulario' => $oFormulario]);
+        return $this->renderForm("pessoa/form.html.twig", [
+            "titulo" => "Adicionar nova Pessoa",
+            "formulario" => $formulario,
+        ]);
     }
 
     /**
      * Retorna o formulário da entidade pessoa
-     * @param Request $oRequisicao,
-     * @param Pessoa $oPessoa,
+     * @param Request $request,
+     * @param Pessoa $pessoa,
      */
-    private function getFormularioFromPessoa(Request $oRequisicao, Pessoa $oPessoa)
-    {
-        $oFormulario = $this->createForm(PessoaType::class, $oPessoa);
-        $oFormulario->handleRequest($oRequisicao);
+    private function getFormularioFromPessoa(
+        Request $request,
+        Pessoa $pessoa
+    ): FormInterface {
+        $formulario = $this->createForm(PessoaType::class, $pessoa);
+        $formulario->handleRequest($request);
 
-        return $oFormulario;
-    }
-
-    /**
-     * Verifica se a condição do formulário permite a execução no banco de dados
-     * @param FormInterface $oFormulario
-     * @return bool
-     */
-    private function permiteExecucaoBanco(FormInterface $oFormulario): bool
-    {
-        return $oFormulario->isSubmitted() && $oFormulario->isValid();
-    }
-
-    /**
-     * Aplica as validações da regra de negócio sob o registro 
-     * @param Pessoa $oPessoa
-     * @param EntityManagerInterface $oEm
-     * @return bool
-     */
-    private function validaInformacoesRegistroInclusao(Pessoa $oPessoa, EntityManagerInterface $oEm)
-    {
-        return $this->verificaCpfIsValidoInclusao($oPessoa, $oEm);
-    }
-
-    /**
-     * Verifica se o CPF informado não está associado a outro registro
-     * @param Pessoa $oPessoa
-     * @param EntityManagerInterface $oEm
-     * @return bool
-     */
-    private function verificaCpfIsValidoInclusao(Pessoa $oPessoa, EntityManagerInterface $oEm)
-    {
-        $xRetorno = $oEm->getRepository(Pessoa::class)->findOneBy([
-            'cpf' => $oPessoa->getCpf()
-        ]);
-        return $xRetorno == null;
+        return $formulario;
     }
 
     /**
      * Método que realiza a alteração de uma pessoa
      * @param int $id
-     * @param Request $oRequisicao
-     * @param EntityManagerInterface $oEm
-     * @param PessoaRepository $oPessoaRepo
+     * @param Request $request
+     * @param EntityManagerInterface $entityManager
+     * @param PessoaRepository $pessoaRepository
      */
-    public function editar($id, Request $oRequisicao, EntityManagerInterface $oEm, PessoaRepository $oPessoaRepo): Response
-    {
-        $oPessoa     = $oPessoaRepo->find($id);
-        $oFormulario = $this->getFormularioFromPessoa($oRequisicao, $oPessoa);
-        if ($this->permiteExecucaoBanco($oFormulario)) {
-            if ($this->validaInformacoesRegistroAlteracao($oPessoa, $oEm)) {
-                $oEm->flush();
-                $this->addFlash('info', 'Registro alterado com sucesso!');
+    public function editar(
+        $id,
+        Request $request,
+        PessoaService $pessoaService,
+        EntityManagerInterface $entityManager,
+        PessoaRepository $pessoaRepository
+    ): Response {
+        $pessoa = $pessoaRepository->find($id);
+        $formulario = $this->getFormularioFromPessoa($request, $pessoa);
+
+        if ($formulario->isSubmitted() && $formulario->isValid()) {
+            if (!$pessoaService->validaFormatoCpf($pessoa->getCpf())) {
+                $this->addFlash(
+                    "aviso",
+                    "O formato do CPF inserido não é válido. Para continuar a alteração, informe outro CPF."
+                );
+            } elseif (!$this->verificaCpfExiste($pessoa, $entityManager)) {
+                $this->addFlash(
+                    "aviso",
+                    "Já existe um registro cadastrado com o CPF: {$pessoa->getCpf()}. Para continuar a alteração, informe outro CPF."
+                );
             } else {
-                $this->addFlash('aviso', "Já existe um registro cadastrado com o CPF: {$oPessoa->getCpf()}. Para continuar a alteração, informe outro CPF.");
+                $entityManager->flush();
+                $this->addFlash("info", "Registro alterado com sucesso!");
             }
         }
 
-        return $this->renderForm(self::pathFormCadastroPessoa, ['titulo' => 'Alterar dados Pessoa', 'formulario' => $oFormulario]);
-    }
-
-    /**
-     * Valida as regras de negócio definidas no contexto de alteração
-     * @param Pessoa $oPessoa
-     * @param EntityManagerInterface $oEm
-     * @return bool
-     */
-    private function validaInformacoesRegistroAlteracao(Pessoa $oPessoa, EntityManagerInterface $oEm): bool
-    {
-        return $this->verificaCpfIsValidoAlteracao($oPessoa, $oEm);
-    }
-
-    /**
-     * Verifica se o CPF informado está associado a outro registro além do presente no formulário de alteração
-     * @param Pessoa $oPessoa
-     * @param EntityManagerInterface $oEm
-     * @return bool
-     */
-    private function verificaCpfIsValidoAlteracao(Pessoa $oPessoa, EntityManagerInterface $oEm): bool
-    {   
-        $xRetorno = $oEm->getRepository(Pessoa::class)->findBy([
-            'cpf' => $oPessoa->getCpf()
+        return $this->renderForm("pessoa/form.html.twig", [
+            "titulo" => "Alterar dados Pessoa",
+            "formulario" => $formulario,
         ]);
-        
-        return is_array($xRetorno) ? count($xRetorno) <= 1 : true;
+    }
+
+    /**
+     * Verifica se o CPF informado não está associado a outro registro
+     * @param Pessoa $pessoa
+     * @param EntityManagerInterface $entityManager
+     * @return bool
+     */
+    private function verificaCpfExiste(
+        Pessoa $pessoa,
+        EntityManagerInterface $entityManager
+    ): bool {
+        $resultado = $entityManager->getRepository(Pessoa::class)->findOneBy([
+            "cpf" => $pessoa->getCpf(),
+        ]);
+
+        if (
+            $resultado != null ||
+            ($resultado && $resultado->getCpf() == $pessoa->getCpf())
+        ) {
+            return false;
+        }
+
+        return true;
     }
 
     /**
      * Deleta o registro informado
      * @param int $id
-     * @param EntityManagerInterface $oEm
-     * @param PessoaRepository $oPessoaRepo
+     * @param EntityManagerInterface $entityManager
+     * @param PessoaRepository $pessoaRepository
      * @return Response
      */
-    public function excluir($id, EntityManagerInterface $oEm, PessoaRepository $oPessoaRepo): Response
-    {
-        $oPessoa = $oPessoaRepo->find($id);
-        $oEm->remove($oPessoa);
-        $oEm->flush();
+    public function excluir(
+        $id,
+        EntityManagerInterface $entityManager,
+        PessoaRepository $pessoaRepository
+    ): Response {
+        $pessoa = $pessoaRepository->find($id);
+        $entityManager->remove($pessoa);
+        $entityManager->flush();
 
-        return $this->redirectToRoute('pessoa_consultar');
+        return $this->redirectToRoute("pessoa_consultar");
     }
 }
